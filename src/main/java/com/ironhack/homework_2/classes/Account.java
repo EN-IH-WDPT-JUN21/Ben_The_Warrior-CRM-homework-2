ab8f9a0a222d9c01469b6afc7ac76eee53548ddf
ShaunRly <shaundr@hotmail.co.uk>
2021-07-15 13:36:56 +0100

    Barebones Data Classes and Enums

--- src/main/java/com/ironhack/homework_2/classes/Account.java
+++ src/main/java/com/ironhack/homework_2/classes/Account.java
@@ -1,7 +1,78 @@
 package com.ironhack.homework_2.classes;
+import com.ironhack.homework_2.enums.*;
 
+import java.util.ArrayList;
 import java.util.List;
+import java.util.UUID;
+
 
 public class Account {
+    private UUID id;
+    private Industry industry;
+    private int employeeCount;
+    private String city;
+    private String country;
+    private ArrayList<Contact> contactList;
+    private ArrayList<Opportunity> opportunityList;
+
+    public Account(Industry industry, int employeeCount, String city, String country, Contact contact, Opportunity opportunity){
+        setId(UUID.randomUUID());
+        setEmployeeCount(employeeCount);
+        setCity(city);
+        setCountry(country);
+        ArrayList<Contact> contactList = new ArrayList<Contact>();
+        contactList.add(contact);
+        ArrayList<Opportunity> opportunityList = new ArrayList<Opportunity>();
+        opportunityList.add(opportunity);
+    }
+
+    public UUID getId() {
+        return id;
+    }
+
+    public void setId(UUID id) {
+        this.id = id;
+    }
+
+    public Industry getIndustry() {
+        return industry;
+    }
+
+    public void setIndustry(Industry industry) {
+        this.industry = industry;
+    }
+
+    public int getEmployeeCount() {
+        return employeeCount;
+    }
+
+    public void setEmployeeCount(int employeeCount) {
+        this.employeeCount = employeeCount;
+    }
+
+    public String getCity() {
+        return city;
+    }
+
+    public void setCity(String city) {
+        this.city = city;
+    }
+
+    public String getCountry() {
+        return country;
+    }
+
+    public void setCountry(String country) {
+        this.country = country;
+    }
+
+    public ArrayList<Contact> getContactList() {
+        return contactList;
+    }
+
+
+    public ArrayList<Opportunity> getOpportunityList() {
+        return opportunityList;
+    }
 
 }
--- src/main/java/com/ironhack/homework_2/classes/Opportunity.java
+++ src/main/java/com/ironhack/homework_2/classes/Opportunity.java
@@ -1,5 +1,52 @@
 package com.ironhack.homework_2.classes;
+import com.ironhack.homework_2.enums.*;
+
+import java.util.UUID;
+
 
 public class Opportunity {
+    private UUID id;
+    private Product product;
+    private int quantity;
+    private Contact decisionMaker;
+    private Status status;
 
+    public Opportunity(Product product, int quantity, Contact decisionMaker, Status status){
+        setProduct(product);
+        setQuantity(quantity);
+        setDecisionMaker(decisionMaker);
+        setStatus(status);
+    }
+
+    public Product getProduct() {
+        return product;
+    }
+
+    public void setProduct(Product product) {
+        this.product = product;
+    }
+
+    public int getQuantity() {
+        return quantity;
+    }
+
+    public void setQuantity(int quantity) {
+        this.quantity = quantity;
+    }
+
+    public Contact getDecisionMaker() {
+        return decisionMaker;
+    }
+
+    public void setDecisionMaker(Contact decisionMaker) {
+        this.decisionMaker = decisionMaker;
+    }
+
+    public Status getStatus() {
+        return status;
+    }
+
+    public void setStatus(Status status) {
+        this.status = status;
+    }
 }
